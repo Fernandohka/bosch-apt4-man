2a872d53bda5f030703238bf9d0f3513d9856d14
Fernandohka <fernando2005andrade@gmail.com>
2024-09-06 14:12:22 -0300

    features produtos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/prop-types */
-export const Card = (props) => {
+
+import style from './Card.module.css'
+
+export const Card = ({id, name, desc, value, image, categoria, status}) => {
   return(
-      <div>
-          <h1>{props.name}</h1>
-          <h2>{props.desc}</h2>
-          <p>{props.value}</p>
-          <img src={props.image} alt={props.name} width={150} height={"auto"}/>
+    <div className={style.Card}>
+        <div className={status ? style.BolinhaVerde: style.BolinhaVermelha}></div>
+        <h1>{name}</h1>
+        <h2>{value}</h2>
+        <p>{desc}</p>
+        <img src={image} alt={name} width={150} height={"auto"}/>
       </div>
   )
 }
