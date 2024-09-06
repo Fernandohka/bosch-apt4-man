0a6d21832e8be3682121aed30784179b4323995d
Fernandohka <fernando2005andrade@gmail.com>
2024-09-06 17:21:24 -0300

    mapa

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import { api } from "./api/rmApi"
 import style from './App.module.css'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 function App() {
   const [show, setShow] = useState("")
@@ -82,9 +86,17 @@ function App() {
      {show === "map" &&
         <>
       <h2>Mapa</h2>
-          <div>
-              mapa aqui
-          </div>
+      <MapContainer center={[-25.4248819,-49.273013]} zoom={15} scrollWheelZoom={false} style={{width: "400px", height: "400px"}}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-25.4248819,-49.273013]}>
+          <Popup>
+            Senai
+          </Popup>
+        </Marker>
+      </MapContainer>
          </>
       }
     </div>
